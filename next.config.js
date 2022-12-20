9f469aecf4e3969e14088148d4f310f28889fcf9
Mourya Prasanth <mouryaprasanthp@gmail.com>
2022-12-20 17:32:05 +0530

    update env variable names

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-
 const basePath = process.env.NODE_ENV === 'production' ? '' : process.env.NEXT_PUBLIC_BASE_PATH;
 
 module.exports = {
@@ -10,19 +9,18 @@ module.exports = {
   assetPrefix: basePath,
   trailingSlash: true,
   images: {
-      path: `${basePath}/_next/image`,
-      dangerouslyAllowSVG: true,
-      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    path: `${basePath}/_next/image`,
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   publicRuntimeConfig: {
-      degaAPIKey: process.env.NEXT_PUBLIC_DEGA_API_KEY,
-      spaceId: process.env.NEXT_PUBLIC_SPACE_ID,
-      apiUrl: process.env.NEXT_PUBLIC_API_URL,
-      kratosURL: process.env.NEXT_PUBLIC_KRATOS_URL,
-      kavachProfileURL: process.env.NEXT_PUBLIC_KAVACH_PROFILE_URL,
-      siteURL: process.env.NEXT_PUBLIC_SITE_URL,
-      basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    degaAPIKey: process.env.NEXT_PUBLIC_DEGA_API_KEY,
+    spaceId: process.env.NEXT_PUBLIC_DEGA_SPACE_ID,
+    apiUrl: process.env.NEXT_PUBLIC_DEGA_API_URL,
+    kratosURL: process.env.NEXT_PUBLIC_KRATOS_URL,
+    kavachProfileURL: process.env.NEXT_PUBLIC_KAVACH_PROFILE_URL,
+    siteURL: process.env.NEXT_PUBLIC_SITE_URL,
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   },
-  serverRuntimeConfig: {
-  },
+  serverRuntimeConfig: {},
 };
