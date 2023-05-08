92e44909f6e8aa7588fbebb27300a30d659a69f6
Chakradhar B <chakradhar2711@gmail.com>
2023-05-08 23:49:37 +0530

    update api url

--- pages/sitemap-categories/[page].js
+++ pages/sitemap-categories/[page].js
@@ -10,7 +10,6 @@ function generateSiteMap(categories) {
 
   <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
 
-    <!--We manually set the two URLs we know already-->
     ${categories
       .map((category) => {
         return `
--- store/client.js
+++ store/client.js
@@ -4,7 +4,7 @@ import getConfig from 'next/config';
 
 
 export const client = new ApolloClient({
-  uri: 'https://stag-dega-api.factly.in/query',
+  uri: 'process.env.DEGA_API_URL',
   headers: {
     'X-Dega-API-Key': process.env.DEGA_API_KEY,
     'X-Space': parseInt(process.env.SPACE_ID),
