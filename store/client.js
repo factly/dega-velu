824bf236f196db2c1889e33c262fe9273939fe82
Chakradhar B <chakradhar2711@gmail.com>
2023-05-16 18:16:08 +0530

    minor change in api url

--- store/client.js
+++ store/client.js
@@ -4,7 +4,7 @@ import getConfig from 'next/config';
 
 
 export const client = new ApolloClient({
-  uri: 'process.env.DEGA_API_URL',
+  uri: 'https://stag-dega-api.factly.in/query',
   headers: {
     'X-Dega-API-Key': process.env.DEGA_API_KEY,
     'X-Space': parseInt(process.env.SPACE_ID),
